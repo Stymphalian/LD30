43d82283d6fba0a45e0dae8867b81c2d84148ff8
Stymphalian <saturnslight@gmail.com>
2014-08-25 14:01:56 -0700

    fixed problem with web.js script

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require("express");
 var server = express();
 var port = Number(process.env.PORT || 3000)
 server.use(express.static(__dirname));
-server.listen(port,'127.0.0.1',function(){
-   console.log("Server started on 127.0.0.1:" + port);
+server.listen(port,function(){
+   //console.log("Server started on 127.0.0.1:" + port);
 });
 
